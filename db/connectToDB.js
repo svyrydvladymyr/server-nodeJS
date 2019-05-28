378dde7ad23e334847e4aebc49d5e06806fefe5a
Володимир Свирид <vsvyryd@ifnmu.edu.ua>
2019-05-28 22:10:07 +0300

    deploy to herroku

--- db/connectToDB.js
+++ db/connectToDB.js
@@ -1,20 +1,20 @@
-let mysql = require('mysql');
-let con, db_config = {
-    host: "localhost",
-    user: "root",
-    password: "kalcifer1988",
-    database: "node"
-  };
+// let mysql = require('mysql');
+// let con, db_config = {
+//     host: "localhost",
+//     user: "root",
+//     password: "kalcifer1988",
+//     database: "node"
+//   };
 
-con = mysql.createConnection(db_config);
-con.connect((err) => {
-    if (err) throw errorData(err, res);
-});    
+// con = mysql.createConnection(db_config);
+// con.connect((err) => {
+//     if (err) throw errorData(err, res);
+// });    
 
-setInterval(function () {
-    con.query('SELECT 1');
-}, 10000);
+// setInterval(function () {
+//     con.query('SELECT 1');
+// }, 10000);
 
-module.exports = {
-    con
-}  
+// module.exports = {
+//     con
+// }  
--- server.js
+++ server.js
@@ -16,20 +16,22 @@ http.createServer((req, res) => {
             res.writeHead(200, {'Content-Type': (req.url == '/') ? 'text/html': typeFile});
             stream.pipe(res); 
         break; 
-        case 'POST':     
-            if (chackPostRoutes(req.url)){
-                if (res.statusCode === 200) {
-                    let obj = '';
-                    req.on('data', data => obj += data);
-                    req.on('error', error => errorServer(error, res));    
-                    req.on('end', () =>{((obj, res) => {
-                        if (trueJson(obj) !== false){
-                            switch (req.url){
-                                case '/addToDB': addToDB(trueJson(obj), res); break;
-                                case '/updateDB': updateDB(trueJson(obj), res); break;}
-                        } else {errorData(`Invalid JSON object..!`, res)}})(obj, res)}); 
-                } else {errorData(`ERROR CONNECTION..! --> ${res.statusCode} -- ${res.statusMessage}`, res)};
-            } else if (req.url === '/') {errorData('Empty POST routes..!', res);
-            } else {errorData('Unknown POST routes..!', res)};   
+        case 'POST': 
+        console.log(req.method);
+            
+            // if (chackPostRoutes(req.url)){
+            //     if (res.statusCode === 200) {
+            //         let obj = '';
+            //         req.on('data', data => obj += data);
+            //         req.on('error', error => errorServer(error, res));    
+            //         req.on('end', () =>{((obj, res) => {
+            //             if (trueJson(obj) !== false){
+            //                 switch (req.url){
+            //                     case '/addToDB': addToDB(trueJson(obj), res); break;
+            //                     case '/updateDB': updateDB(trueJson(obj), res); break;}
+            //             } else {errorData(`Invalid JSON object..!`, res)}})(obj, res)}); 
+            //     } else {errorData(`ERROR CONNECTION..! --> ${res.statusCode} -- ${res.statusMessage}`, res)};
+            // } else if (req.url === '/') {errorData('Empty POST routes..!', res);
+            // } else {errorData('Unknown POST routes..!', res)};   
         break;   
     }}).listen(8080, () => {console.log('Server working...')});
