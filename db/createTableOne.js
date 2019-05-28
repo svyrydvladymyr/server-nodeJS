4ddcd458814462480207f1affe63820c2f300fa3
Володимир Свирид <vsvyryd@ifnmu.edu.ua>
2019-05-29 02:01:44 +0300

    deploy to herroku v2

--- db/createTableOne.js
+++ db/createTableOne.js
@@ -1,11 +1,9 @@
 let con = require('./connectToDB').con;
   
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+
     let sql = "CREATE TABLE customersOne (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("Table created");
-    });
+
 });
