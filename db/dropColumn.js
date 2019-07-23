973031719cedcacda08ee6e0cf93ffc92eba5304
Володимир Свирид <vsvyryd@ifnmu.edu.ua>
2019-07-24 02:09:50 +0300

    finish friend list

--- db/dropColumn.js
+++ db/dropColumn.js
@@ -1,4 +1,4 @@
 let con = require('./connectToDB').con;
 let sql = `ALTER TABLE userssettings DROP COLUMN avasettings`;            
-    con.query(sql, function (err, result) {if (err) throw err; console.log("Column Alert")});
+    con.query(sql, function (err, result) {if (err) throw err; console.log("Column Drop")});
 
