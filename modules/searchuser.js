770eb183bb691addfc9ec7ddff71e35dc3b17b9d
Володимир Свирид <vsvyryd@ifnmu.edu.ua>
2019-07-24 02:23:56 +0300

    finish friend list

--- modules/searchuser.js
+++ modules/searchuser.js
@@ -6,7 +6,7 @@ let {translit, token, clienttoken} = require('./service');
 let searchUser = (req, res) => {
     let searchuser = req.body.searchuser;
     let transl = translit(searchuser);
-    let sql = `SELECT U.name, U.surname, U.userid, U.ava, S.avasettings FROM users U INNER JOIN userssettings S on U.userid=S.userid WHERE U.name LIKE '${searchuser}%' OR U.surname LIKE '${searchuser}%' OR U.name LIKE '${transl}%' OR U.surname LIKE '${transl}%' OR U.userid LIKE '${transl}%'`;
+    let sql = `SELECT U.name, U.surname, U.userid, U.ava, U.avasettings FROM users U INNER JOIN userssettings S on U.userid=S.userid WHERE U.name LIKE '${searchuser}%' OR U.surname LIKE '${searchuser}%' OR U.name LIKE '${transl}%' OR U.surname LIKE '${transl}%' OR U.userid LIKE '${transl}%'`;
     con.query(sql, function (err, result) {
         if (err) {
             console.log("err", err);
