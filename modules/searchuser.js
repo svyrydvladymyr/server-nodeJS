008d0a31570d90b90cc53327da547c41e4756286
Володимир Свирид <vsvyryd@ifnmu.edu.ua>
2019-08-13 23:46:30 +0300

    fix

--- modules/searchuser.js
+++ modules/searchuser.js
@@ -179,32 +179,35 @@ let showfriends = (req, res) => {
                     if (result.length === 0){
                         res.send({"res":"", "myid":"", "name":"", "surname":"", "kilk":"", "status":""});
                     }    
-                    for(let i = 1; i <= kilkfriends; i++){
-                        let sqlselreg = `SELECT regtype FROM users WHERE userid = '${result[i-1].friendid}';`;
-                        con.query(sqlselreg, function (err, result) {
-                            if (err) {
-                                console.log("err", err);
-                            } else {    
-                                let type = (result == '') ? undefined : result[0].regtype;
-                                let email = ((type === null) || (type === undefined)) ? `` : `${type}`;
-                                let sqlsel = `SELECT userid, name, surname, ${email}email, phone, country, town, ava, avasettings FROM users WHERE userid = '${renameres[i-1].friendid}';`;
-                                con.query(sqlsel, function (err, result) {
-                                    if (err) {
-                                        console.log("err", err);
-                                        res.send({"error":err});
-                                    } else {    
-                                        let emailready = (result == '') ? '' : result[0][`${email}email`];
-                                        if (result[0] !== undefined){
-                                            masfriends.push({"friendvisit":renameres[i-1].friendvisit, "userid":result[0].userid, "name":result[0].name, "surname":result[0].surname, "ava":result[0].ava, "avasettings":result[0].avasettings, "email":emailready, "birthday":result[0].birthday, "phone":result[0].phone, "country":result[0].country, "town":result[0].town, "profession":result[0].profession, "education":result[0].education})
-                                        }       
-                                        if (i === kilkfriends) {                                            
-                                            res.send({"res":masfriends, "myid":renameres[0].userid, "name":renameres[0].name, "surname":renameres[0].surname, "kilk":kilk, "status":renameres[0].friendstatus});
+                    setTimeout(() => {
+                        for(let i = 1; i <= kilkfriends; i++){
+                            let sqlselreg = `SELECT regtype FROM users WHERE userid = '${result[i-1].friendid}';`;
+                            con.query(sqlselreg, function (err, result) {
+                                if (err) {
+                                    console.log("err", err);
+                                } else {    
+                                    let type = (result == '') ? undefined : result[0].regtype;
+                                    let email = ((type === null) || (type === undefined)) ? `` : `${type}`;
+                                    let sqlsel = `SELECT userid, name, surname, ${email}email, phone, country, town, ava, avasettings FROM users WHERE userid = '${renameres[i-1].friendid}';`;
+                                    con.query(sqlsel, function (err, result) {
+                                        if (err) {
+                                            console.log("err", err);
+                                            res.send({"error":err});
+                                        } else {    
+                                            let emailready = (result == '') ? '' : result[0][`${email}email`];
+                                            if (result[0] !== undefined){
+                                                masfriends.push({"friendvisit":renameres[i-1].friendvisit, "userid":result[0].userid, "name":result[0].name, "surname":result[0].surname, "ava":result[0].ava, "avasettings":result[0].avasettings, "email":emailready, "birthday":result[0].birthday, "phone":result[0].phone, "country":result[0].country, "town":result[0].town, "profession":result[0].profession, "education":result[0].education})
+                                            }       
+                                            if (i === kilkfriends) {                                            
+                                                res.send({"res":masfriends, "myid":renameres[0].userid, "name":renameres[0].name, "surname":renameres[0].surname, "kilk":kilk, "status":renameres[0].friendstatus});
+                                            }
                                         }
-                                    }
-                                });                            
-                            }
-                        });
-                    }
+                                    });                            
+                                }
+                            });
+                        }
+                    }, 50);
+
                 }
             }); 
         }
