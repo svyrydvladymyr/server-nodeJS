1864400b8b17bb0d4d51a180f3f9470ba4ef50af
Володимир Свирид <vsvyryd@ifnmu.edu.ua>
2019-11-12 00:07:15 +0200

    fix

--- db/connectToDB.js
+++ db/connectToDB.js
@@ -1,29 +1,29 @@
 let mysql = require('mysql');
 
-// let con, db_config = {
-//     driver: "mysql",
-//     host: "eu-cdbr-west-02.cleardb.net",
-//     user: "bb3b6857c7c94b",
-//     password: "abe128b6",
-//     charset: 'utf8',
-//     collation: 'utf8_unicode_ci',
-//     prefix: '',
-//     strict: false,
-//     database: "heroku_c286770ec73016d"
-//   };
-
-
 let con, db_config = {
     driver: "mysql",
-    host: "localhost",
-    user: "root",
-    password: "",
+    host: "eu-cdbr-west-02.cleardb.net",
+    user: "bb3b6857c7c94b",
+    password: "abe128b6",
     charset: 'utf8',
     collation: 'utf8_unicode_ci',
     prefix: '',
     strict: false,
-    database: "kalcifer33333"
+    database: "heroku_c286770ec73016d"
   };
+
+
+// let con, db_config = {
+//     driver: "mysql",
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     charset: 'utf8',
+//     collation: 'utf8_unicode_ci',
+//     prefix: '',
+//     strict: false,
+//     database: "kalcifer33333"
+//   };
 
 let dbName = db_config.database;
 
